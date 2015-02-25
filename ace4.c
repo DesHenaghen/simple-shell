15e7e37a8512b3cdc2a31b174e1f1712a240e77e
Chris Bean <chrisdwbean@gmail.com>
2015-02-25 13:22:55 +0000

    Changed the way arguments are passed to Execute(): arguments are passed in an array in which the first element is the name of the command we want to run and the following elements are the arguments that will be passed to that command. Implemented some basic error checking for the Execute() function: ensure that the array passed to it contains at least one element and that the name of the command we want to run is null-terminated. It must be null-terminated to prevent any potential mis-haps with strcmp. Print some error messages if anything is wrong.

--- ace4.c
+++ ace4.c
@@ -3,20 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The name of our shell! */
+#define SHELLNAME "shell"
+/* The number of elements in an array */
+#define LEN(array) sizeof(array)/sizeof(array[0])
+
 /* Execute looks for the command specified by filename.
  * The order it looks for the command is:
  * 	1. Among built-in commands,
  * 	2. In the PATH.
  *
- * Arguments are passed in the same way that arguments
- * are passed to C's execv function (from unistd.h), around
- * which this function is a wrapper.
- *
- * Returns 0 if successful, otherwise returns 1. */
-int Execute(const char *filename, char *const argv[]) {
-/* TODO: Search for built-in command. */
-/* TODO: Search for command in path. */
-	return 0;
+ * Arguments are passed as an array where the first element
+ * is the name of the command we want to run and the following
+ * elements are arguments to that command. */
+void Execute(const char *argv[]) {
+	/* Let's make sure there's actually something in the array! */
+	if (LEN(argv) == 0) {
+		fprintf(stderr,"%s: error: no arguments given to Execute().\n",SHELLNAME);
+		return;
+	}
+
+	/* The name of the command we want to run */
+	const char* filename = argv[0];
+	
+	/* Ensure our file name is properly null-terminated. */
+	if (filename[LEN(filename)] != '\0') {
+		/* It isn't?! Let us know on stderr. */
+		fprintf(stderr,"%s: error: filename is not null terminated.\n",SHELLNAME);
+		return;
+	}
+	
+	/* Built-in commands */
+	
+	/* TODO: The rest of the built in commands. */
+
+	/* TODO: Search for command in path. */
+
+	/* We couldn't find it. */
+	printf("%s: %s: command not found.\n",SHELLNAME,filename);
+	return;
 }
 
 int main() {
