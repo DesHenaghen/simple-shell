49be879051d72586ac49aeee21fb15453f0801f6
Desmond Henaghen <deshenaghen@yahoo.co.uk>
2015-02-26 10:22:20 +0000

    Fixing another git error... again

--- ace4.c
+++ ace4.c
@@ -1,4 +1,4 @@
-/* ACE 4 */
+* ACE 4 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,22 +9,28 @@
 #define SHELLNAME "shell"
 /* The number of elements in an array */
 #define LEN(array) sizeof(array)/sizeof(array[0])
-
+/* Are two strings equal? */
+#define EQ(str1,str2) (!strcmp(str1,str2))
 #define UNCHANGED 0
 #define MAXIN 512 /* MAXIN is the maximum number of input characters */
 
 #define DELIM " \n\t|><&;" /* DELIM is the string containing all delimiters for tokens */
 
+/* Return the PATH environment variable */
+char* getpath() {
+	return getenv("PATH");
+}
+
 char* get_input(char directory[]) { 
 	static char input[MAXIN]; /* declared as static so it's not on the stack */
-  
-  	fgets(input,MAXIN,stdin); /* fgets as scanf() can't handle blank lines */
-  	/* First of all check if it was a blank line, i.e. just a '\n' input...*/
-  	while('\n' == input[0]) {    /*check if input is valid - i.e. not blank*/
-		printf("%s >", directory); 	/*prompt user for other input*/
-		fgets(input,MAXIN,stdin); 	/* get user input */ 
-  	} /* TO ASK - why does fgets not return a char* in this case? */
- 
+  	do {
+	printf("%s >", directory); 
+  	fgets(input,MAXIN,stdin); /* get user input */   
+	}
+	/* fgets as scanf() can't handle blank lines */
+  	/* check if it was a blank line, i.e. just a '\n' input...*/
+  	while('\n' == input[0]);   /*check if input is valid - i.e. not blank*/
+			
   	/* If we get to this point it there has to be input so just return it. */ 
   	return(input);
 }
@@ -69,7 +75,7 @@ int Execute(char *argv[]) {
 	if (LEN(argv) == 0) {
 		fprintf(stderr,"%s: error: no arguments given to Execute().\n",SHELLNAME);
 		return(1);
-	}
+	}exit
 
 
 	/* The name of the command we want to run */
@@ -82,13 +88,17 @@ int Execute(char *argv[]) {
 		return(1);
 	}*/
 	
-	/* Built-in commands */
+	/* Internal commands */
+	/* TODO: interna; commands as another function */
 	/* exit*/
-	if(!strcmp(filename,"exit")) {
+	if(EQ(filename,"exit")) {
 		exit(0);
+	} else if (EQ(filename,"getpath")) {
+		printf("%s\n",getpath());
+		return 0;
 	}
 	
-	/* TODO: The rest of the built in commands. */
+	/* TODO: The rest of the internal commands. */
 	/* TODO: Search for command in path. */
 
 	/* We couldn't find it. */
@@ -109,3 +119,4 @@ int main() {
 	}
 	return 0;
 }
+
